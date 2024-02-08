97136951a5d6e793aa9de8d82cf00f24bc268f8a
Adrielly Inocencio <adrielly.inoccencio@gmail.com>
2024-02-08 13:58:03 -0300

    Commit

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.texto1}>Adrielly Souza Inocencio - Armário 2 PC 13</Text>
-      <Text style={styles.texto2}>Adrielly Souza Inocencio - Armário 2 PC 13</Text>
+      <Text style={styles.titulo}>Conchas</Text>
+      <Text style={styles.texto2}>As conchas são órgãos duros, de formação calcária, presentes na parte externa do corpo dos moluscos. Como os moluscos não possuem ossos na parte interna do corpo, as conchas possuem a função de garantir a proteção do corpo destes animais. As conchas podem ser de diversos tipos: formadas por uma peça única, com formato de cone, com formato de espiral, arredondada, fechada, aberta, duas partes ligadas por uma pequena membrana, entre outras.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,17 +20,14 @@ const styles = StyleSheet.create({
     justifyContent: 'left',
   },
 
-  texto1: {
-    fontSize: 18,
-    fontWeight: 'bold',
+  titulo: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: "bold",
   },
 
   texto2: {
-    marginTop: 10, // Adiciona espaço entre os dois textos
-    backgroundColor: '#b10bd6', // Cor de fundo diferente
-    borderColor: '#fa0202', // Cor da borda
-    color: '#FFFFFF',// Cor da letra
-    borderWidth: 1, // Largura da borda
+    marginTop: 10,
     padding: 10,
     fontSize: 16,
   },

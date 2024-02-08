574144ac237db9e11cfed20e0267ff3f8a175aae
Adrielly Inocencio <adrielly.inoccencio@gmail.com>
2024-02-08 09:38:20 -0300

    Commmit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Adrielly Souza Inocencio - Armário 2 PC 13</Text>
       <StatusBar style="auto" />
     </View>
   );

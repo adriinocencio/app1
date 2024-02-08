c787211fb9f6fdf3db3d0a61898dd6570d24aad2
Adrielly Inocencio <adrielly.inoccencio@gmail.com>
2024-02-08 10:41:10 -0300

    Adição de um segundo elemento "text"

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Adrielly Souza Inocencio - Armário 2 PC 13</Text>
+      <Text style={styles.texto1}>Adrielly Souza Inocencio - Armário 2 PC 13</Text>
+      <Text style={styles.texto2}>Adrielly Souza Inocencio - Armário 2 PC 13</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'left',
     justifyContent: 'left',
+  },
+
+  texto1: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+  texto2: {
+    marginTop: 10, // Adiciona espaço entre os dois textos
+    backgroundColor: '#b10bd6', // Cor de fundo diferente
+    borderColor: '#fa0202', // Cor da borda
+    color: '#FFFFFF',// Cor da letra
+    borderWidth: 1, // Largura da borda
+    padding: 10,
+    fontSize: 16,
   },
 });
